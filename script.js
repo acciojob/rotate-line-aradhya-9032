53c974bb6f8e41171c8ddea058f6eca45846fe73
Aradhya <120170628+aradhya-9032@users.noreply.github.com>
2023-05-25 14:55:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+    const line = document.getElementById('line');
+
+    // Set the initial rotation angle
+    let rotationAngle = 0;
+
+    // Function to rotate the line
+    function rotateLine() {
+      rotationAngle += 1; // Increase the rotation angle by 1 degree
+      line.style.transform = `rotate(${rotationAngle}deg)`;
+
+      // Request the next animation frame
+      requestAnimationFrame(rotateLine);
+    }
+
+    // Start rotating the line
+    rotateLine();
